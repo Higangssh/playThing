84f6eedc91130b1d05a1ac62ac463a0e2d49aada
sonsanghee <sonsanghee3@gmail.com>
2024-07-22 23:33:01 +0900

    first commit

---
+++ src/main/kotlin/com/the/playting/PlayTingApplication.kt
@@ -0,0 +1,11 @@
+package com.the.playting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlayTingApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlayTingApplication>(*args)
+}
